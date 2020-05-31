6c42dfe9d17466651c74cdec41c91e7044934d9c
Antonio <zolotuhin87@gmail.com>
2020-06-01 01:56:30 +0300

    :heavy_minus_sign: :heavy_plus_sign: Set webhook to receive bot updates

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-const Telegraf = require('telegraf');
 require('./server');
-require('dotenv').config();
-const { addGroceryItem, getGroceryItems } = require('./src/commands.js');
-const { formatDbResponse } = require('./src/helpers.js');
-const { enableNotificationsForUser } = require('./src/cron.js');
+const bot = require('./src/bot');
+const webhookUrl = process.env.WEBHOOK_URL;
+const token = process.env.TELEGRAM_BOT_TOKEN;
 
-// Instantiate a new bot
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+bot.setWebHook(webhookUrl + token);
 
-bot.start((ctx) => ctx.reply(`Hello ${ctx.from.first_name}. Welcome to Grocery Life.\n
-Type /help for more information about how to use the bot` 
-));
-
-bot.command('add', (ctx) => {
-    addGroceryItem(ctx);
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1];
+    bot.sendMessage(chatId, `Hello ${msg.chat.first_name}! You sent: ${resp}`);
 });
-
-bot.command('get', (ctx) => {
-    ctx.reply(formatDbResponse(getGroceryItems(ctx.from.id)));
-});
-
-bot.command('cron', (ctx) => {
-    enableNotificationsForUser(ctx.from.id);
-});
-
-bot.command('help', (ctx) => {
-    ctx.replyWithMarkdown(`Use the following commands to control your bot:\nTODO`);
-});
-
-bot.launch()
--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const app = express();
 require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const bot = require('./src/bot');
 
 app.get('/', (req, res) => {
     res.json({
         message: 'Grocery Life Bot was launched successfully',
         status: 200
     })
+});
+
+app.post('/' + process.env.TELEGRAM_BOT_TOKEN, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
 });
 
 app.listen(process.env.PORT, () => {
